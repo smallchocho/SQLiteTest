ff8135b5010a4536bcb2475a2b8159de1ecdd2d7
smallchocho <bigchocho@gmail.com>
2018-05-30 18:19:35 +0800

    Refactor

--- SQLiteTest/SQLiteManager.swift
+++ SQLiteTest/SQLiteManager.swift
@@ -9,11 +9,14 @@
 import Foundation
 class SQLiteManager{
     static let sqlitePath = NSHomeDirectory() + "/Documents/sqlite3.db"
+    static let students = "students"
+    static let studentsColumsInfo = ["id integer primary key autoincrement","name text","height double"]
     let sqlitePath:String
     init?(path:String){
         sqlitePath = path
         db = self.openSQLiteDatabase(path: sqlitePath)
         if db == nil { return nil }
+        guard creatSQLiteTable(name: SQLiteManager.students, columsInfo: SQLiteManager.studentsColumsInfo) else{ return nil }
     }
     
     var db:OpaquePointer?
@@ -41,12 +44,37 @@ class SQLiteManager{
         print("建立表格失敗")
         return false
     }
-    func creatData(name:String,rowInfo:[String:String])->Bool{
+    
+    func creatStudentsDefaultData()->Bool{
+        return self.creatData(name: SQLiteManager.students, rowInfo: ["name":"Justin","height":189.9])
+    }
+    
+    
+    func creatData(name:String,rowInfo:[String:Any])->Bool{
         var statement:OpaquePointer?
+        var infoValues:[String] = []
+        for value in rowInfo.values{
+            switch value {
+            case let v as String:
+                let str = "'\(v)'"
+                infoValues.append(str)
+            case let v as Double:
+                let str = String(v)
+                infoValues.append(str)
+            case let v as Int:
+                let str = String(v)
+                infoValues.append(str)
+            case let v as Float:
+                let str = String(v)
+                infoValues.append(str)
+            default :
+                assert(false, "此格式尚未支援")
+            }
+        }
         let sql = "insert into \(name) "
             + "(\(rowInfo.keys.joined(separator: ","))) "
             + "values "
-            + "(\(rowInfo.values.joined(separator: ",")))"
+            + "(\(infoValues.joined(separator: ",")))"
         guard sqlite3_prepare_v2(db, (sql as NSString).utf8String, -1, &statement, nil) == SQLITE_OK else{
             print("新增資料失敗")
             return false
@@ -60,7 +88,7 @@ class SQLiteManager{
         return true
         
     }
-    func readData(name:String,cond:String?,order:String?)->OpaquePointer?{
+    func readData(name:String,cond:String?,order:String?,completion:(Bool,OpaquePointer?)->()){
         var statement:OpaquePointer?
         var sql = "select * from \(name)"
         if let condition = cond {
@@ -69,8 +97,15 @@ class SQLiteManager{
         if let orderBy = order{
             sql += " order by \(orderBy)"
         }
-        sqlite3_prepare_v2(db, (sql as NSString).utf8String, -1, &statement, nil)
-        return statement
+        let sqlitePrepare = sqlite3_prepare_v2(db, (sql as NSString).utf8String, -1, &statement, nil)
+        guard sqlitePrepare == SQLITE_OK else{
+            completion(false,nil)
+            return
+        }
+        while sqlite3_step(statement) == SQLITE_ROW {
+            completion(true,statement)
+        }
+        sqlite3_finalize(statement)
 
     }
     func updateDate(name:String,cond :String?, rowInfo :[String:String])->Bool{
@@ -94,6 +129,7 @@ class SQLiteManager{
             print("更新資料失敗")
             return false
         }
+        sqlite3_finalize(statement)
         print("更新資料成功")
         return true
     }
@@ -109,6 +145,7 @@ class SQLiteManager{
             return false
         }
         if sqlite3_step(statement) == SQLITE_DONE {
+            sqlite3_finalize(statement)
             print("刪除資料成功")
             return true
         }
